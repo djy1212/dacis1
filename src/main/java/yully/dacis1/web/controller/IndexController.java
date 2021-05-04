1b9d418e978a2dcaf33aacd236405f9f59b62396
DJY <djy1212@gmail.com>
2021-05-04 16:25:59 +0900

    add resources

---
+++ src/main/java/yully/dacis1/web/controller/IndexController.java
@@ -0,0 +1,15 @@
+package yully.dacis1.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+    @GetMapping("/jinjumungo")
+    public String jinjumungo() {
+        System.out.println("jinjumungo");
+        return "jinjumungo";
+    }
+}
