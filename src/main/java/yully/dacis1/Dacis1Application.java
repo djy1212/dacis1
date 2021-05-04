37632e97912f6d090af97f27047bda071265f639
DJY <djy1212@gmail.com>
2021-05-04 16:24:09 +0900

    Initial commit

---
+++ src/main/java/yully/dacis1/Dacis1Application.java
@@ -0,0 +1,13 @@
+package yully.dacis1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dacis1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dacis1Application.class, args);
+	}
+
+}
---
+++ src/test/java/yully/dacis1/Dacis1ApplicationTests.java
@@ -0,0 +1,13 @@
+package yully.dacis1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dacis1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
