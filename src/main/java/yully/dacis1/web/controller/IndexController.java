37178de41b06a7de90dab42dcc6725f418fe51bc
DJY <djy1212@gmail.com>
2021-05-04 21:34:00 +0900

    convert java version add index page

--- src/main/java/yully/dacis1/web/controller/IndexController.java
+++ src/main/java/yully/dacis1/web/controller/IndexController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        System.out.println("jinjumungo");
+        return "jinjumungo";
+    }
     @GetMapping("/jinjumungo")
     public String jinjumungo() {
         System.out.println("jinjumungo");
