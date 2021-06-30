7701605ce7b9acc593e37430d715e8ef7ce7a457
dongjun-yeong <djy1212@gmail.com>
2021-06-30 14:36:47 +0900

    change backimage add sangbong

--- src/main/java/yully/dacis1/web/controller/IndexController.java
+++ src/main/java/yully/dacis1/web/controller/IndexController.java
@@ -4,17 +4,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.text.SimpleDateFormat;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
     @GetMapping("/")
     public String index() {
-        System.out.println("jinjumungo");
-        return "jinjumungo";
+        return calcAndOutDate("jinjumungo");
     }
     @GetMapping("/jinjumungo")
     public String jinjumungo() {
-        System.out.println("jinjumungo");
-        return "jinjumungo";
+        return calcAndOutDate("jinjumungo");
+    }
+    @GetMapping("/sangbong")
+    public String sangbong() {
+        return calcAndOutDate("sangbong");
+    }
+
+    public String calcAndOutDate(String name){
+        SimpleDateFormat dateFormat = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
+        String time = dateFormat.format (System.currentTimeMillis());
+        System.out.println(time + " " + name);
+        return name;
     }
 }
