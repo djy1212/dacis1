bdb688943827b229735c3c0f3a87292661388e8d
dongjun-yeong <djy1212@gmail.com>
2021-08-29 21:08:57 +0900

    mod namgaram

--- src/main/java/yully/dacis1/web/controller/IndexController.java
+++ src/main/java/yully/dacis1/web/controller/IndexController.java
@@ -13,39 +13,43 @@ public class IndexController {
 
     private int count = 0;
     private int count2 = 0;
+    SimpleDateFormat dateFormat = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
+
     @GetMapping("/")
     public String index() {
         return calcAndOutDate("jinjumungo");
     }
+
     @GetMapping("/jinjumungo")
     public String jinjumungo() {
         return calcAndOutDate("jinjumungo");
     }
+
     @GetMapping("/sangbong")
     public String sangbong() {
         return calcAndOutDate("sangbong");
     }
+
     @GetMapping("/namgaram")
     public String namegaram() {
         return calcAndOutDate2("namgaram");
     }
+
     @GetMapping("/count")
     public String count(Model model) {
         //{{count}}
-        model.addAttribute("count1", count);
+        model.addAttribute("count", count);
         model.addAttribute("count2", count2);
         return "count";
     }
 
 
     public String calcAndOutDate(String name){
-        SimpleDateFormat dateFormat = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
         String time = dateFormat.format (System.currentTimeMillis());
         System.out.println(time + " " + name + " count1:" + ++count);
         return name;
     }
     public String calcAndOutDate2(String name){
-        SimpleDateFormat dateFormat = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
         String time = dateFormat.format (System.currentTimeMillis());
         System.out.println(time + " " + name + " count2:" + ++count2);
         return name;
